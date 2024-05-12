7a3470611e703c39be0b2d2f15d3afcf913ea8a3
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-12 10:20:24 -0400

    submenu - hide & column layout

--- app.js
+++ app.js
@@ -36,3 +36,52 @@ ${links
   </article>`;
   })
   .join("");
+
+linkBtns.forEach((btn) => {
+  btn.addEventListener("mouseover", function (e) {
+    const text = e.currentTarget.textContent;
+    const tempBtn = e.currentTarget.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+
+    const tempPage = sublinks.find(({ page }) => page === text);
+    console.log(tempPage);
+
+    if (tempPage) {
+      const { page, links } = tempPage;
+      submenu.classList.add("show");
+      submenu.style.left = `${center}px`;
+      submenu.style.top = `${bottom}px`;
+
+      // OPTIONAL
+      let columns = "col-2";
+      if (links.length === 3) {
+        columns = "col-3";
+      }
+      if (links.length > 3) {
+        columns = "col-4";
+      }
+      submenu.innerHTML = `<section>
+      <h4>${page}</h4>
+      <div class="submenu-center ${columns}">
+      ${links
+        .map((link) => {
+          return `<a href="${link.url}">
+        <i class="${link.icon}"></i>${link.label}
+        </a>`;
+        })
+        .join("")}
+      </div>
+      </section>`;
+    }
+  });
+});
+
+hero.addEventListener("mouseover", function (e) {
+  submenu.classList.remove("show");
+});
+nav.addEventListener("mouseover", function (e) {
+  if (!e.target.classList.contains("link-btn")) {
+    submenu.classList.remove("show");
+  }
+});
